7a16152077e05b4446014013927c09c54edfc7c1
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2025-08-02 01:31:55 +0200

    Changed settings due to changed app name

--- android/app/src/main/java/com/otazky/rlp/MainActivity.kt
+++ android/app/src/main/java/com/otazky/rlp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.otazky.app
+package com.otazky.rlp
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/otazky/rlp/MainApplication.kt
+++ android/app/src/main/java/com/otazky/rlp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.otazky.app
+package com.otazky.rlp
 
 import android.app.Application
 import android.content.res.Configuration
