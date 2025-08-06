7849746a9d55803501a1389857be213f64fee383
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2025-08-06 15:01:54 +0200

    Welcome screen dark mode fix

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -3,13 +3,15 @@ import ThemedText from "@/components/ThemedText";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 import { SetStateAction, useEffect, useState } from "react";
-import { Button } from "react-native-paper";
+import { View } from "react-native";
+import { Button, useTheme } from "react-native-paper";
 import { TextInput } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function WelcomeScreen() {
   const [username, setUsername] = useState("");
   const [loaded, setLoaded] = useState(false);
   const router = useRouter();
+  const { colors } = useTheme();
 
   useEffect(() => {
     loadUsername();
@@ -29,37 +31,39 @@ export default function WelcomeScreen() {
   return (
     <>
       {loaded ? (
-        <SafeAreaView
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            padding: 20,
-            gap: 16,
-          }}
-        >
-          <ThemedText
-            className="text-4xl"
-            style={{ textAlign: "center", marginBottom: 20 }}
+        <View style={{ backgroundColor: colors.background, flex: 1 }}>
+          <SafeAreaView
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              padding: 20,
+              gap: 16,
+            }}
           >
-            Vítejte
-          </ThemedText>
-          <TextInput
-            mode="outlined"
-            style={{ width: "100%", fontFamily: "Inter" }}
-            label="Uživatelské jméno"
-            value={username}
-            onChangeText={(text: SetStateAction<string>) => setUsername(text)}
-          />
-          <Button
-            mode="contained"
-            disabled={!username}
-            labelStyle={{ fontFamily: "Inter" }}
-            onPress={login}
-          >
-            Pokračovat
-          </Button>
-        </SafeAreaView>
+            <ThemedText
+              className="text-4xl"
+              style={{ textAlign: "center", marginBottom: 20 }}
+            >
+              Vítejte
+            </ThemedText>
+            <TextInput
+              mode="outlined"
+              style={{ width: "100%", fontFamily: "Inter" }}
+              label="Uživatelské jméno"
+              value={username}
+              onChangeText={(text: SetStateAction<string>) => setUsername(text)}
+            />
+            <Button
+              mode="contained"
+              disabled={!username}
+              labelStyle={{ fontFamily: "Inter" }}
+              onPress={login}
+            >
+              Pokračovat
+            </Button>
+          </SafeAreaView>
+        </View>
       ) : (
         <LoadingScreen description="Načítání" />
       )}
